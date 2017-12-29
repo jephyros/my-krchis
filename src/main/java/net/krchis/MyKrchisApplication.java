7360d04e705635c8799f1c25a69d9382308e5cb3
ADMIN-NB\CHIS <kr.chis@gmail.com>
2017-12-29 14:56:01 +0900

    최초 커밋

---
+++ src/main/java/net/krchis/MyKrchisApplication.java
@@ -0,0 +1,12 @@
+package net.krchis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKrchisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyKrchisApplication.class, args);
+	}
+}
