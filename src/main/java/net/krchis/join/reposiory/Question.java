4d400827f239ce3e9cdbf55afeea033399913cbe
ADMIN-NB\CHIS <kr.chis@gmail.com>
2018-01-02 09:44:09 +0900

    qna 상세보기완료

--- src/main/java/net/krchis/join/reposiory/Question.java
+++ src/main/java/net/krchis/join/reposiory/Question.java
@@ -1,30 +1,47 @@
 package net.krchis.join.reposiory;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Question {
 	@Id
 	@GeneratedValue
 	private Long id;
-		
-	
-	private String writer;		
-	private String title;	
+
+	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
+	private User writer;
+	private String title;
 	private String contents;
-	
+	private LocalDateTime createDate;
+
 	public Question() {
-		
+
 	}
-	
-	public Question(String writer, String title, String contents) {
+
+	public Question(User writer, String title, String contents) {
 		super();
 		this.writer = writer;
 		this.title = title;
 		this.contents = contents;
+		this.createDate = LocalDateTime.now();
+	}
+
+	public String getCreateDateFormat() {
+		if (createDate == null) {
+			return "";
+		}
+		return createDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
 	}
 
 }
